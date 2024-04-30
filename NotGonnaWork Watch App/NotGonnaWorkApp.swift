3598ddad5cec6abd23b54d5fecf09098340ff2f7
Alfie Le Feuvre <alfielefeuvre@icloud.com>
2024-04-30 20:15:34 +0100

    Initial Commit

---
+++ NotGonnaWork Watch App/NotGonnaWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotGonnaWorkApp.swift
+//  NotGonnaWork Watch App
+//
+//  Created by Alfie Le Feuvre on 30/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct NotGonnaWork_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

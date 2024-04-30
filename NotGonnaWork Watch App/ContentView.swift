11974f95dbde5b9ba52b88116d9e91c419e98598
Alfie Le Feuvre <alfielefeuvre@icloud.com>
2024-04-30 20:15:34 +0100

    Initial Commit

---
+++ NotGonnaWork Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotGonnaWork Watch App
+//
+//  Created by Alfie Le Feuvre on 30/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Goodbye cruel, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
